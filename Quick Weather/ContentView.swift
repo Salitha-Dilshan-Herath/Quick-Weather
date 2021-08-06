00b259c07468d17beaf3699642ba96efaa63e852
Salitha <salitha.herath@spemai.com>
2021-08-06 15:31:01 +0530

    inital commit

---
+++ Quick Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Quick Weather
+//
+//  Created by Spemai-Macbook on 2021-08-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, worldx!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Quick Weather/Quick_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quick_WeatherApp.swift
+//  Quick Weather
+//
+//  Created by Spemai-Macbook on 2021-08-06.
+//
+
+import SwiftUI
+
+@main
+struct Quick_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
